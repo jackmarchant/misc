cbe281361a85fd49e93e18ba3cddb99bd6d34e68
Jack Marchant <jack@expert360.com>
2017-01-05 09:41:15 +1100

    Add to mix.exs to publish the package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Misc.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package(),
+     description: description()]
   end
 
   # Configuration for the OTP application
@@ -27,6 +29,22 @@ defmodule Misc.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    """
+    Some arithmetic functions in Elixir to start to get used to the syntax.
+    Also includes a SumList module, which uses recursion to sum numbers in a list.
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     name: :misc,
+     files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Jack Marchant"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/jackmarchant/misc"}]
   end
 end
