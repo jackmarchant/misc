13d41e8b9a267f162d9b250138e3fb8f6b45389e
Jack Marchant <jack@expert360.com>
2017-01-08 13:34:47 +1100

    Added docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Misc.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package(),
+     description: description()]
   end
 
   # Configuration for the OTP application
@@ -15,6 +17,21 @@ defmodule Misc.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
+  end
+
+  defp description do
+    """
+    Some arithmetic functions in Elixir to start to get used to the syntax. Also includes a SumList module, which uses recursion to sum numbers in a list.
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     name: :misc,
+     files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Jack Marchant"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/jackmarchant/misc"}]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +44,6 @@ defmodule Misc.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
